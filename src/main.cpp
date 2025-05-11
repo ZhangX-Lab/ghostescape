d5c91cd65d0d42cdb71ebd59784264320f43972b
ZhangX-Lab <896327117@qq.com>
2025-05-11 13:49:53 +0000

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "core/game.h"
-
+#include <iostream>
 int main(int, char**) {
     auto& game = Game::GetInstance();
+    std::cout << "GhostEscape running" << std::endl;
     game.init("GhostEscape", 1280, 720);
     game.run();
     return 0;
